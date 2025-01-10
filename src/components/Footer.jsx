6c7022d223b4facc741b18ee45f706a929d9826d
developedbychris <killchrisu@gmail.com>
2025-01-10 14:40:04 -0500

    added bluesky footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,13 @@ const Footer = () => {
       <img src={TKLogo} alt="Toukou Logo" className="w-20 select-none hover:cursor-pointer" onClick={handleClick}/>
       <div className="flex-col flex items-center justify-center">
         <a href="https://x.com/ToukouApp" target="_blank">
-          <h1 className="mb-2 md:mb-7 text-lg md:text-2xl xl:text-3xl hover:text-TwitterBlue duration-200">Twitter</h1>
+          <h1 className="mb-2 md:mb-4 text-lg md:text-2xl xl:text-xl hover:text-TwitterBlue duration-200">Twitter</h1>
+        </a>
+        <a href="https://bsky.app/profile/toukou.co" target="_blank">
+          <h1 className="mb-2 md:mb-4 text-lg md:text-2xl xl:text-xl hover:text-BlueskyBlue duration-200">Bluesky</h1>
         </a>
         <a href="https://anilist.co/" target="_blank">
-          <h1 className="text-lg md:text-2xl xl:text-3xl hover:text-AniListBlue duration-200">Ani<span className="text-AniListBlue">List</span></h1>
+          <h1 className="text-lg md:text-2xl xl:text-xl hover:text-AniListBlue duration-200">Ani<span className="text-AniListBlue">List</span></h1>
         </a>
       </div>
     </footer>
